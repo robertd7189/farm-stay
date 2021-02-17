a25abcee6312fa6bb4575253221cb77211da8f5c
Robert Dooley <robert.dooley@fdmgroup.com>
2021-02-18 08:57:28 +1000

    First working war on AWS EB

--- FarmWorkSpringBoot/src/main/java/com/fdm/FarmWorkSpringBoot/FarmWorkSpringBootApplication.java
+++ FarmWorkSpringBoot/src/main/java/com/fdm/FarmWorkSpringBoot/FarmWorkSpringBootApplication.java
@@ -2,9 +2,10 @@ package com.fdm.FarmWorkSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FarmWorkSpringBootApplication {
+public class FarmWorkSpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FarmWorkSpringBootApplication.class, args);
